44db87d07dbbcb7a76c0065c5553dd92d321cddb
christian <chran554@hotmail.com>
2024-02-16 12:37:49 +0100

    fix generated resource variable location

--- internal/app/resources/resources.go
+++ internal/app/resources/resources.go
@@ -1,3 +1,7 @@
-//go:generate fyne bundle --output resources_generated.go --package resources --name ThisIsFyneImageResource images/this_is_fyne.jpg
+//go:generate fyne bundle --output resources_generated.go --package resources --name thisIsFyneImageResource images/this_is_fyne.jpg
 
 package resources
+
+var (
+	ThisIsFyneImageResource = thisIsFyneImageResource
+)
